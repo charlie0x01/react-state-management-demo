cfe07636be0ce4a7bd9ce54fb926485b7e098429
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-03-27 01:00:03 +0500

    feat: created cakeSlice for managing cake state

---
+++ redux-toolkit-demo/features/cake/cakeSlice.js
@@ -0,0 +1,33 @@
+// 1. we need createSlice from @redux/toolkit
+const createSlice = require("@reduxjs/toolkit").createSlice;
+
+// 2. init state for cakeSlice
+const initState = {
+  numOfCake: 10,
+};
+
+// 3. now we need to create slice for cake
+// createSlice takes object as argument with these properties
+// I. name of the
+// II. initial State
+// III. reducers
+// further, reducers is an object of actions (functions) that we perform on the state
+
+const cakeSlice = createSlice({
+  name: "cake",
+  initialState: initState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfCake -= 1;
+    },
+    restocked: (state, action) => {
+      state.numOfCake += action.payload;
+    },
+  },
+});
+
+// createSlice automatically create actions and action generators for the given reducer functions
+
+// export the slice and actions
+module.export = cakeSlice.reducer;
+module.export.cakeAction = cakeSlice.actions;
