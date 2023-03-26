f4e383d5471902cd5edaa1372e166ca2ac35fc97
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-03-27 02:53:41 +0500

    feat: example of asyncThunk

--- redux-toolkit-demo/app/store.js
+++ redux-toolkit-demo/app/store.js
@@ -2,9 +2,10 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
-// const iceCreamReducer = require("../features/icecream/icecreamSlice");
+const iceCreamReducer = require("../features/icecream/icecreamSlice");
+const userReducer = require("../features/user/userSlice");
 // exampel with extra reducer for icecream
-const iceCreamReducer = require("../features/icecream/with_extra_reducers");
+// const iceCreamReducer = require("../features/icecream/with_extra_reducers");
 
 const logger = reduxLogger.createLogger();
 
@@ -13,6 +14,7 @@ const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: iceCreamReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
--- redux-toolkit-demo/index.js
+++ redux-toolkit-demo/index.js
@@ -3,23 +3,27 @@ const store = require("./app/store");
 // import cakeActions
 const { cakeActions } = require("./features/cake/cakeSlice");
 const { iceCreamActions } = require("./features/icecream/icecreamSlice");
+const { fetchUsers } = require("./features/user/userSlice");
 
 // subscribe to the store
 const unsub = store.subscribe(() => {});
 
 // dispatch actions
 // order some cakes
-store.dispatch(cakeActions.ordered());
-store.dispatch(iceCreamActions.ordered(2));
-store.dispatch(cakeActions.ordered());
-store.dispatch(iceCreamActions.ordered(2));
-store.dispatch(cakeActions.ordered());
-store.dispatch(iceCreamActions.ordered(2));
-store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(iceCreamActions.ordered(2));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(iceCreamActions.ordered(2));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(iceCreamActions.ordered(2));
+// store.dispatch(cakeActions.ordered());
 
-// restock cakes
-store.dispatch(cakeActions.restocked(2));
-store.dispatch(iceCreamActions.restocked(4));
+// // restock cakes
+// store.dispatch(cakeActions.restocked(2));
+// store.dispatch(iceCreamActions.restocked(4));
+
+// fetch users
+store.dispatch(fetchUsers());
 
 // unsub to the store
-unsub();
+// unsub();
